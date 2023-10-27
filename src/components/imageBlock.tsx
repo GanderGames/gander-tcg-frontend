09e36ca6d29bf3ad867e7d2ae9ec3a496c4bfccd
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2023-10-27 17:30:21 +0200

    leon en dylan

---
+++ src/components/imageBlock.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface props{
+    img: string
+}
+
+export default function (prop:props) {
+  return (
+    <div>
+        <Image className='rounded-3xl'  src={prop.img} alt="epic" width={500} height={100}/>
+    </div>
+  )
+}
